2e5bfba5cdc88b9f20a4f6e372c373e31c2f68d3
MauroRivero-Globant <mauro.rivero@globant.com>
2021-09-16 22:49:59 -0300

    d

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3030;
 const path = require('path');
-app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/views/', 'home.html'));
   })
