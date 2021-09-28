fb10ee67df925546c4b2846da8419e275a8982e4
mauro1892 <mauro.river92@gmail.com>
2021-09-28 06:17:57 -0300

    router works

--- routes/routerMain.js
+++ routes/routerMain.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'./views/', 'home.html'));
+    res.sendFile(path.join(__dirname, '../views/home.html'));
 })
 
 module.exports = router;
