aa58147c86207f275ea21e1677f4375a82c212ed
mauro1892 <mauro.river92@gmail.com>
2021-09-28 06:12:18 -0300

    first attemp to make a router

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3030;
-const path = require('path');
+
+const routerMain = require('./routes/routerMain');
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/views/', 'home.html'));
-  })
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/', 'home.html'));
-})
-app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/', 'login.html'));
-})
-app.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname+'/views/', 'register.html'));
-})
+
+app.use("/", routerMain);
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname+'/views/', 'home.html'));
+//   })
+// app.get('/home', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/views/', 'home.html'));
+// })
+// app.get('/login', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/views/', 'login.html'));
+// })
+// app.get('/register', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/views/', 'register.html'));
+// })
 
 
 app.listen(port, ()=>{
---
+++ routes/routerMain.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'./views/', 'home.html'));
+})
+
+module.exports = router;
